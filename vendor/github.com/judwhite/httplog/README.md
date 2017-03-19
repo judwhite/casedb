d2ec5a70bba1fceb5dc3cf3a7bc0d64d062f8374
Jud White <github@judsonwhite.com>
2017-03-18 22:05:45 -0500

    vendor: add httplog

---
+++ vendor/github.com/judwhite/httplog/README.md
@@ -0,0 +1,192 @@
+# httplog
+Structured, leveled logging for your HTTP server. Returns structs as JSON and shuts down gracefully.
+
+Wrapped up in a package so I don't need to copy this code over and over again.
+
+Graceful shutdown packages:
+- https://github.com/facebookgo/grace
+- https://github.com/tylerb/graceful
+
+Logging packages:
+- https://github.com/Sirupsen/logrus
+- https://github.com/natefinch/lumberjack
+- https://github.com/judwhite/logrjack
+
+# Example Log Output
+
+```
+GET http://localhost:5678/ping
+GET http://localhost:5678/add?a=1&b=2
+GET http://localhost:5678/wait?t=2
+
+time="2016-03-04T18:17:07-06:00" level=info msg=OK bytes_sent=2 http_status=200 method=GET remote_addr="[::1]:55915" time_taken=0 uri="/ping"
+time="2016-03-04T18:17:14-06:00" level=info msg=OK bytes_sent=37 http_status=200 method=GET remote_addr="[::1]:55915" req_a=1 req_b=2 result_sum=3 time_taken=2 uri="/add?a=1&b=2"
+time="2016-03-04T18:17:30-06:00" level=info msg=OK bytes_sent=2 http_status=200 method=GET remote_addr="[::1]:55915" time_taken=2000 uri="/wait?t=2"
+
+GET http://localhost:5678/add?a=1&b=badparam
+GET http://localhost:5678/wait
+
+time="2016-03-04T18:33:02-06:00" level=warning msg="strconv.ParseInt: parsing \"badparam\": invalid syntax" bytes_sent=31 callstack="httplog_test/main.go:89, httplog/server.go:102" http_status=400 method=GET remote_addr="[::1]:45908" req_a=1 req_b=badparam time_taken=0 uri="/add?a=1&b=badparam"
+time="2016-03-04T18:33:08-06:00" level=warning msg="strconv.ParseInt: parsing \"\": invalid syntax" bytes_sent=23 callstack="httplog_test/main.go:128, httplog/server.go:102" http_status=400 method=GET remote_addr="[::1]:45908" time_taken=0 uri="/wait"
+
+GET http://localhost:5678/panic
+
+time="2016-03-04T18:21:21-06:00" level=error msg="runtime error: integer divide by zero" bytes_sent=0 callstack="httplog/server.go:92, runtime/panic.go:426, runtime/panic.go:27, runtime/signal_windows.go:166, httplog_test/main.go:112, httplog/server.go:102" http_status=500 method=GET remote_addr="[::1]:55915" time_taken=0 uri="/panic"
+```
+
+# Example
+
+Note: In practice you'd probably want to use a mux package such as https://github.com/gorilla/mux.
+
+```go
+package main
+
+import (
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"github.com/judwhite/httplog"
+	log "github.com/judwhite/logrjack"
+)
+
+type webServer struct {
+	httplog.Server
+}
+
+type fields map[string]interface{}
+
+func main() {
+	start := time.Now()
+
+	log.Setup(log.Settings{
+		MaxSizeMB:   100,
+		MaxAgeDays:  30,
+		WriteStdout: true,
+	})
+
+	svr := webServer{
+		Server: httplog.Server{
+			FormatJSON:      true,
+			ShutdownTimeout: 2 * time.Second,
+			// tell httplog how we want new log entries created so it can use our
+			// custom formatters and be written to the correct destinations.
+			NewLogEntry: func() httplog.Entry { return log.NewEntry() },
+		},
+	}
+
+	go func() {
+		svr.Start()
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, os.Kill)
+	<-signalChan
+
+	log.Info("Shutting down...")
+	svr.Shutdown()
+	log.Infof("Shutdown complete. Total uptime: %v", time.Since(start))
+}
+
+func (svr *webServer) Start() {
+	const addr = ":5678"
+
+	http.HandleFunc("/add", svr.Handle(addHandler))
+	http.HandleFunc("/ping", svr.Handle(pingHandler))
+	http.HandleFunc("/panic", svr.Handle(panicHandler))
+	http.HandleFunc("/wait", svr.Handle(waitHandler))
+	http.HandleFunc("/", svr.Handle(catchAllHandler))
+
+	log.Infof("Listening on %s...", addr)
+
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+type addResponse struct {
+	A      int `json:"a"`
+	B      int `json:"b"`
+	Result int `json:"result"`
+}
+
+func addHandler(r *http.Request, log httplog.Entry) (interface{}, int, error) {
+	u, err := url.ParseRequestURI(r.RequestURI)
+	if err != nil {
+		log.AddCallstack()
+		return nil, http.StatusBadRequest, err
+	}
+	m := u.Query()
+
+	queryParamA := m.Get("a")
+	queryParamB := m.Get("b")
+
+	log.AddFields(fields{
+		"req_a": queryParamA,
+		"req_b": queryParamB,
+	})
+
+	a, err := strconv.Atoi(queryParamA)
+	if err != nil {
+		log.AddCallstack()
+		return "specify ?a=[int]&b=[int] to add", http.StatusBadRequest, err
+	}
+	b, err := strconv.Atoi(queryParamB)
+	if err != nil {
+		log.AddCallstack()
+		return "specify ?a=[int]&b=[int] to add", http.StatusBadRequest, err
+	}
+
+	sum := a + b
+	log.AddField("result_sum", sum)
+
+	resp := addResponse{
+		A:      a,
+		B:      b,
+		Result: sum,
+	}
+
+	return resp, 200, nil
+}
+
+func pingHandler(r *http.Request, log httplog.Entry) (interface{}, int, error) {
+	return "OK", 200, nil
+}
+
+func panicHandler(r *http.Request, requestLogger httplog.Entry) (interface{}, int, error) {
+	var a int
+	for i := 0; i < 1; i++ {
+		a += 1 / i // cause a panic
+	}
+	return a, 200, nil
+}
+
+func waitHandler(r *http.Request, log httplog.Entry) (interface{}, int, error) {
+	u, err := url.ParseRequestURI(r.RequestURI)
+	if err != nil {
+		log.AddCallstack()
+		return nil, http.StatusBadRequest, err
+	}
+	m := u.Query()
+
+	t := m.Get("t")
+	secs, err := strconv.Atoi(t)
+	if err != nil {
+		log.AddCallstack()
+		return "specific ?t= in seconds", http.StatusBadRequest, err
+	}
+
+	start := time.Now()
+	time.Sleep(time.Duration(secs) * time.Second)
+	return time.Since(start).String(), 200, nil
+}
+
+func catchAllHandler(r *http.Request, log httplog.Entry) (interface{}, int, error) {
+	return "404", http.StatusNotFound, nil
+}
+```
+
+# License
+
+MIT
---
+++ vendor/github.com/judwhite/httplog/server.go
@@ -0,0 +1,324 @@
+package httplog
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// Server provides functionality for:
+//
+//   - Structured, leveled logging per request via the Handle method
+//   - Error and panic handling
+//   - Clean shutdown
+//
+// Server is intended to be embedded in another struct, though it
+// can be used standalone.
+//
+// See the Handle method for behavior details.
+type Server struct {
+	stopped         int32
+	openConnections int32
+
+	// ShutdownTimeout defines the duration to wait for outstanding requests
+	// to complete before the Shutdown method returns. The default is 30s.
+	ShutdownTimeout time.Duration
+	// FormatJSON determines whether non-byte and non-string responses are
+	// formatted with MarshalIndent (when true) or Marshal (when false. The
+	// default is false.
+	FormatJSON bool
+	// NewLogEntry is a "func() Entry" field. Set this property to specify
+	// how new log entries are created. This field must be set to integrate
+	// with an outside logging package.
+	NewLogEntry func() Entry
+}
+
+// Entry is implemented by a log entry.
+type Entry interface {
+	AddField(key string, value interface{})
+	AddFields(fields map[string]interface{})
+	AddError(err error)
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+}
+
+// Handler contains the handler name and handler function.
+type Handler struct {
+	Name string
+	Func loggedHandler
+}
+
+type loggedHandler func(r *http.Request, requestLogger Entry) (Response, error)
+
+// Response contains the body, status, and HTTP headers to return.
+type Response struct {
+	Body    interface{}
+	Status  int
+	Headers []Header
+}
+
+// Header contains the name/value pair of a response HTTP header.
+type Header struct {
+	Name  string
+	Value string
+}
+
+// Handle accepts a Handler and returns a function which
+// can be passed to http.HandleFunc.
+//
+// If the Shutdown method has been called Handle responds with
+// StatusServiceUnavailable (503).
+//
+// If the Handler panics it's recovered and the server responds with
+// StatusInternalServerError (500). The callstack is also captured and added
+// to the log.
+//
+// If the response from Handler is a type other than string or
+// []byte the object is serialized as JSON. See the FormatJSON field.
+//
+// Returning an error from Handler does not modify the status code. The
+// error itself will be written to the log.
+//
+// After the response has been written to the client WriteHTTPLog is called.
+func (svr *Server) Handle(handler Handler) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bytesSent := 0
+		status := 0
+		start := time.Now()
+		requestLogger := svr.newEntry()
+
+		var decOpenConnections bool
+		var err error
+
+		defer func() {
+			if perr := recover(); perr != nil {
+				status = http.StatusInternalServerError
+				w.WriteHeader(status)
+
+				perror, ok := perr.(error)
+				if !ok {
+					perror = fmt.Errorf("%v", perr)
+				}
+
+				err = errors.WithStack(perror)
+			}
+
+			go WriteHTTPLog(handler.Name, requestLogger, r, start, status, bytesSent, err)
+
+			if decOpenConnections {
+				atomic.AddInt32(&svr.openConnections, -1)
+			}
+		}()
+
+		// stopped
+		if atomic.LoadInt32(&svr.stopped) == 1 {
+			status = http.StatusServiceUnavailable
+			return
+		}
+
+		decOpenConnections = true
+		atomic.AddInt32(&svr.openConnections, 1)
+
+		httpResponse, err := handler.Func(r, requestLogger)
+
+		resp := httpResponse.Body
+		status = httpResponse.Status
+		headers := httpResponse.Headers
+
+		if status == 0 {
+			status = 200
+		}
+
+		for _, hdr := range headers {
+			w.Header().Add(hdr.Name, hdr.Value)
+		}
+
+		if resp != nil {
+			var body []byte
+			if respString, ok := resp.(string); ok {
+				body = []byte(respString)
+			} else if respBytes, ok := resp.([]byte); ok {
+				body = respBytes
+			} else {
+				var marshalErr error
+				if svr.FormatJSON {
+					body, marshalErr = json.MarshalIndent(resp, "", "  ")
+				} else {
+					body, marshalErr = json.Marshal(resp)
+				}
+				if marshalErr != nil {
+					if err != nil {
+						err = errors.Wrap(err, fmt.Sprintf("json.Marshal:%s", marshalErr))
+					} else {
+						err = errors.Wrap(marshalErr, "json.Marshal")
+					}
+					status = http.StatusInternalServerError
+				} else {
+					w.Header().Add("Content-Type", "application/json")
+				}
+			}
+
+			w.WriteHeader(status)
+
+			if body != nil {
+				_, writeBodyErr := w.Write(body)
+				if writeBodyErr != nil {
+					if err != nil {
+						err = errors.Wrap(err, fmt.Sprintf("http.ResponseWriter.Write:%s", writeBodyErr))
+					} else {
+						err = errors.Wrap(writeBodyErr, "http.ResponseWriter.Write")
+					}
+				} else {
+					bytesSent = len(body)
+				}
+			}
+		} else {
+			w.WriteHeader(status)
+		}
+	}
+}
+
+// Shutdown attempts a graceful shutdown, waiting for outstanding connections
+// to complete. See ShutdownTimeout.
+func (svr *Server) Shutdown() {
+	atomic.StoreInt32(&svr.stopped, 1)
+
+	deadlineTimeout := svr.ShutdownTimeout
+	if deadlineTimeout == 0 {
+		deadlineTimeout = 30 * time.Second
+	}
+
+	deadline := time.After(deadlineTimeout)
+	ticker := time.NewTicker(100 * time.Millisecond)
+loop:
+	for {
+		entry := svr.newEntry()
+		select {
+		case <-ticker.C:
+			conns := atomic.LoadInt32(&svr.openConnections)
+			if conns > 0 {
+				entry.Infof("waiting for %d connections to close", conns)
+			} else {
+				entry.Info("all connections closed")
+				break loop
+			}
+		case <-deadline:
+			conns := atomic.LoadInt32(&svr.openConnections)
+			if conns > 0 {
+				entry.Errorf("stop deadline %v exceeded; aborting %d connections", deadlineTimeout, conns)
+			}
+			break loop
+		}
+	}
+}
+
+func (svr *Server) newEntry() Entry {
+	newEntryFunc := svr.NewLogEntry
+	if newEntryFunc != nil {
+		return newEntryFunc()
+	}
+	log.Print("*** WARNING *** Set Server.NewLogEntry implementation to use your logging framework. Using fallback logger.")
+	svr.NewLogEntry = func() Entry { return &fallbackLogger{} }
+	return svr.newEntry()
+}
+
+// WriteHTTPLog writes the following keys to the log entry:
+//
+//   bytes_sent           The number of bytes sent in the HTTP response body.
+//   http_status          The HTTP status code returned.
+//   method               GET, POST, PUT, DELETE, etc
+//   remote_addr          The remote IP address.
+//   time_taken           The time taken to complete the request in milliseconds,
+//                        including writing to the client.
+//   uri                  The request URI.
+//
+// The log level is determined by the status code:
+//
+//   status < 400          Info
+//   400 <= status < 500   Warning
+//   status >= 500         Error
+//
+// This function is invoked by Server's Handle method.
+func WriteHTTPLog(handlerName string, entry Entry, r *http.Request, start time.Time, status int, bytesSent int, err error) {
+	timeTakenSecs := float64(time.Since(start)) / 1e9
+
+	labelValues := []string{strconv.Itoa(status), handlerName, r.Method}
+	httpRequestsTotal.WithLabelValues(labelValues...).Inc()
+	httpRequestDurationCounter.WithLabelValues(labelValues...).Observe(float64(timeTakenSecs))
+	httpRequestSizeBytes.WithLabelValues(labelValues...).Observe(float64(r.ContentLength))
+	httpResponseSizeBytes.WithLabelValues(labelValues...).Observe(float64(bytesSent))
+
+	var host string
+	ip, _, splitErr := net.SplitHostPort(r.RemoteAddr)
+	if splitErr != nil {
+		ip = r.RemoteAddr
+		host = r.RemoteAddr
+	} else {
+		host = getHostFromIP(ip)
+	}
+
+	entry.AddFields(map[string]interface{}{
+		"http_status": status,
+		"method":      r.Method,
+		"uri":         r.RequestURI,
+		"bytes_sent":  bytesSent,
+		"ip":          ip,
+		"host":        host,
+		"time_taken":  int64(timeTakenSecs * 1000),
+	})
+
+	msg := http.StatusText(status)
+	if err != nil {
+		entry.AddError(err)
+	}
+
+	if status >= 400 && status < 500 {
+		entry.Warn(msg)
+	} else if status >= 500 {
+		entry.Error(msg)
+	} else {
+		entry.Info(msg)
+	}
+}
+
+var ipHost map[string]string
+var ipHostMtx sync.RWMutex
+
+func init() {
+	ipHost = make(map[string]string)
+}
+
+// GetHostFromAddress gets a host name from an IPv4 address
+func getHostFromIP(ip string) string {
+	ipHostMtx.RLock()
+	entry, ok := ipHost[ip]
+	ipHostMtx.RUnlock()
+
+	if !ok {
+		names, lookupErr := net.LookupAddr(ip)
+		if lookupErr != nil || len(names) == 0 {
+			entry = ip
+		} else {
+			entry = strings.TrimSuffix(names[0], ".")
+		}
+
+		ipHostMtx.Lock()
+		ipHost[ip] = entry
+		ipHostMtx.Unlock()
+	}
+
+	return entry
+}
